e1f14d118dec52b6b00bd45e1794b451f1c6a475
4-brothers <sahil@4brotherstruck.com>
2025-07-27 14:46:34 -0700

    Add src/components/Footer.js 1753652794496

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: '1rem', backgroundColor: '#f8f8f8', textAlign: 'center', marginTop: 'auto' }}>
+      <p style={{ margin: 0 }}>© 2023 Sun Haulers Dump Truck Business</p>
+    </footer>
+  );
+};
+
+export default Footer;
