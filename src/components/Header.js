6f33c10700d58f6022a8a5bfbfdb0f16018f8342
4-brothers <sahil@4brotherstruck.com>
2025-07-27 14:46:35 -0700

    Add src/components/Header.js 1753652795219

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ padding: '1rem', backgroundColor: '#f8f8f8', textAlign: 'center' }}>
+      <h1 style={{ margin: 0, fontSize: '2rem' }}>Sun Haulers</h1>
+    </header>
+  );
+};
+
+export default Header;
